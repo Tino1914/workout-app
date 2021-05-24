121c0913446d171bb7ae276d12556d61e3f669ab
Konstantin Hadjiyankov <constantin_hadjiyankov@abv.bg>
2021-05-24 22:39:54 +0200

    Calendar css and fuction added

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
+
 
 import Grid from '@material-ui/core/Grid';
 
@@ -10,10 +11,35 @@ function Calendar(props) {
 
     const {firebase, authUser} = props;
 
+    /* HOOKS */
+    const [dateObject, setdateObject] = useState(moment());
+    const [showMonthTable, setShowMonthTable] = useState(false);
+
+    /* CALENDAR HEAD */
+    const allMonths = moment.months();
+    const currentMonth = () => dateObject.format("MMMM");
+    const currentYear = () => dateObject.format("YYYY");
+
+    const setMonth = month => {
+        let monthNo = allMonths.indexOf(month);
+        let newDateObject = Object.assign({}, dateObject);
+        newDateObject = moment(dateObject).set("month", monthNo);
+        setdateObject(newDateObject);
+        setShowMonthTable(false);
+    }
+
+    const toggleMonthSelect = () => setShowMonthTable(!showMonthTable);
+
     return (
         <Grid container spacing={3}>
             <Grid item xs={12} md={8} lg={9}>
                     <CalendarHead
+                        allMonths={allMonths}
+                        currentMonth={currentMonth}
+                        currentYear={currentYear}
+                        setMonth={setMonth}
+                        showMonthTable={showMonthTable}
+                        toggleMonthSelect={toggleMonthSelect}
                     />
                     <CalendarBody 
                     />
